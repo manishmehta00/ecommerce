55555203e5b57864df53f30f021aa79adf271217
manishmehta00 <145211372+manishmehta00@users.noreply.github.com>
2024-10-01 18:44:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let bar=document.getElementById('bar');
+let close=document.getElementById('close');
+let navbar=document.getElementById('navbar');
+
+if(bar)
+{
+    bar.addEventListener('click',()=>{
+        navbar.classList.add('active')
+    })
+}
+
+
+if(close){
+    close.addEventListener('click',()=>{
+        navbar.classList.remove('active')
+    })
+}
